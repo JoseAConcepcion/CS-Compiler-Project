33f3b3edf9a01dfcd562dded36c00815a2a9bd77
Jose <concepcijoseantonio@gmail.com>
2024-06-15 15:31:19 -0400

    feat: added cmp files from cps

---
+++ hulkConstants.py
@@ -0,0 +1,8 @@
+reserved_words = ['function', 'let',
+                   'type', 'self', 'inherits', 'protocol', 'extends', 
+                   'in',
+                   'if', 'else', 'elif',
+                   'while', 'for', 
+                   'new', 
+                   ]
+reserved_functions = ['print', 'sqrt' , 'asin' , 'cos', 'exp', 'log', 'rand']
